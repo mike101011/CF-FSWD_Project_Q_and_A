aaa3338f7e732c1b28f688e5b886cb12c6db56f6
Michael Zechner <michael-zechner@gmx.at>
2022-03-01 15:34:28 +0100

    edit-question functionality complete

--- questions/all-questions.php
+++ questions/all-questions.php
@@ -11,7 +11,7 @@ $questionbdy = $msg = "";
 $quesall = "";
 $quesdet = "d-none";
 $class = "d-none";
-$sql = "SELECT * FROM (tags JOIN quetag ON tags.t_id=quetag.fk_t_id JOIN questions ON quetag.fk_q_id=questions.q_id) LEFT JOIN users ON questions.fk_u_id=users.u_id; ";
+$sql = "SELECT * FROM (tags JOIN quetag ON tags.t_id=quetag.fk_t_id JOIN questions ON quetag.fk_q_id=questions.q_id) LEFT JOIN users ON questions.fk_u_id=users.u_id ORDER BY q_id DESC; ";
 $res = mysqli_query($connect, $sql);
 if ($res->num_rows > 0) {
     while ($row = mysqli_fetch_assoc($res)) {
@@ -27,8 +27,21 @@ if ($res->num_rows > 0) {
             $val2 = "Open";
             $resclass = "";
         }
+        if (isset($_SESSION["adm"])) {
+            $questionbdy .= "<div class='mz-question'>
+        <h3>" . $row["q_title"] . "</h3>
+        <h4>Tag: " . $row["title"] . "</h4>
+        <h5>" . $row["q_vote"] . "</h5>
+        <h5 class='" . $resclass . "'>" . $val2 . "</h5>
+        <h5>By " . $val . "</h5>
+        <a href='view-question.php?id=" . $row["q_id"] . "' class='btn btn-primary'>View</a>
+        <a href='edit-question.php?q_id=" . $row["q_id"] . "' class='btn btn-warning'>Edit</a>
+<hr>
+        </div>";
+        } else {
 
-        $questionbdy .= "<div class='mz-question'>
+
+            $questionbdy .= "<div class='mz-question'>
         <h3>" . $row["q_title"] . "</h3>
         <h4>Tag: " . $row["title"] . "</h4>
         <h5>" . $row["q_vote"] . "</h5>
@@ -37,6 +50,7 @@ if ($res->num_rows > 0) {
         <a href='view-question.php?id=" . $row["q_id"] . "' class='btn btn-primary'>View</a>
 <hr>
         </div>";
+        }
     }
 } else {
     $class = "";
@@ -52,7 +66,7 @@ if (isset($_GET["user"])) {
         $quesdet = "";
         $class = "d-none";
         $questionbdy = $msg = "";
-        $sql = "SELECT * FROM (tags JOIN quetag ON tags.t_id=quetag.fk_t_id JOIN questions ON quetag.fk_q_id=questions.q_id) LEFT JOIN users ON questions.fk_u_id=users.u_id WHERE u_id='$u_id'; ";
+        $sql = "SELECT * FROM (tags JOIN quetag ON tags.t_id=quetag.fk_t_id JOIN questions ON quetag.fk_q_id=questions.q_id) LEFT JOIN users ON questions.fk_u_id=users.u_id WHERE u_id='$u_id' ORDER BY q_id DESC; ";
         $res = mysqli_query($connect, $sql);
         if ($res->num_rows > 0) {
             while ($row = mysqli_fetch_assoc($res)) {
